b10f9f97d5bcd86bd9e34454ce6844cbe6b7c236
Petre Stegarus <stegarusp@gmail.com>
2021-04-10 12:53:03 +0300

    tidied up sorted insert

--- tLG.h
+++ tLG.h
@@ -14,14 +14,12 @@ typedef struct celulag
   struct celulag *next, *prev; // adresa celulei urmatoare si anterioare
 } TCelulaG, *TLG;       // tipurile Celula, Lista
 
-typedef int (*TFElem)(void *);        // functie prelucrare element
 typedef int (*TFCmp)(void *, void *); // functie de comparare doua elemente
 typedef void (*TF)(void *);           // functie afisare/eliberare un element
 
 // functii lista generica 
 int InsLG(TLG *, void *, TFCmp);  //- inserare la inceput reusita sau nu (1/0) -
 void Distruge(TLG *aL, TF fe); // distruge lista
-size_t LungimeLG(TLG *);       // numarul de elemente din lista
 // afiseaza elementele din lista, folosind o functie de tip TFAfi
 void Afisare(TLG *, TF);
 
